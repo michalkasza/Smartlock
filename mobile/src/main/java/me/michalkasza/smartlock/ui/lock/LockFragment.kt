9f37411e67a04f67bb6a328d6532e2422d7b6589
Mikey Kasza <michalkasza@protonmail.com>
2018-02-17 23:54:16 +0100

    Binding adapters, data-binding for lock ViewPager

--- mobile/src/main/java/me/michalkasza/smartlock/ui/lock/LockFragment.kt
+++ mobile/src/main/java/me/michalkasza/smartlock/ui/lock/LockFragment.kt
@@ -1,5 +1,30 @@
 package me.michalkasza.smartlock.ui.lock
 
-class LockFragment {
-    
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.michalkasza.smartlock.R
+import me.michalkasza.smartlock.databinding.FragmentLockBinding
+
+class LockFragment: Fragment() {
+    private lateinit var lockViewModel: LockViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val viewBinding: FragmentLockBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_lock_status,
+                container,
+                false)
+
+        lockViewModel = ViewModelProviders.of(this).get(LockViewModel::class.java)
+        viewBinding.viewModel = lockViewModel
+
+        return viewBinding.root
+    }
+
+
 }
---
+++ mobile/src/main/java/me/michalkasza/smartlock/ui/lock/LockViewModel.kt
@@ -0,0 +1,7 @@
+package me.michalkasza.smartlock.ui.lock
+
+import me.michalkasza.smartlock.base.BaseViewModel
+
+class LockViewModel: BaseViewModel() {
+
+}
