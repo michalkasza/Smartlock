65031b86d9246b9fbbafbfe87b37dd25a6ec6b35
Michał <michal@pop-os.localdomain>
2021-06-11 20:29:18 +0200

    Upgraded dependencies, fixed upgrade issues

--- mobile/src/androidTest/java/dev/michalkasza/smartlock/ExampleInstrumentedTest.kt
+++ mobile/src/androidTest/java/dev/michalkasza/smartlock/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("me.michalkasza.smartlock", appContext.packageName)
+        assertEquals("dev.michalkasza.smartlock", appContext.packageName)
     }
 }
--- things/src/androidTest/java/dev/michalkasza/smartlock/ExampleInstrumentedTest.kt
+++ things/src/androidTest/java/dev/michalkasza/smartlock/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("me.michalkasza.smartlock", appContext.packageName)
+        assertEquals("dev.michalkasza.smartlock", appContext.packageName)
     }
 }
